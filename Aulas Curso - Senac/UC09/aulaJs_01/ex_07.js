7ae40a8dc7aaff16e4494c4d9388be558c3cc7c6
Mari <cys.mariana@gmail.com>
2026-03-07 01:12:51 -0300

    Create ex_07.js

---
+++ Aulas Curso - Senac/UC09/aulaJs_01/ex_07.js
@@ -0,0 +1,9 @@
+let temperatura = parseFloat(prompt("Digite a temperatura em celsius"))
+
+if (temperatura > 30){
+    document.writeln("Ta quente")
+} else if (temperatura >= 20 && 30) {
+    document.writeln("Agradavel")
+} else {
+    document.writeln("Frio")
+}
